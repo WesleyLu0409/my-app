8ec50b8787589a833a9f15ad2cc19aebb8b9e9f0
WesleyLu0409 <april.9.2002.wesley@gmail.com>
2022-08-04 20:56:56 +0800

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,29 @@
+<script>
+	let count=0;
+	function countup() {
+		count +=1;
+	}
+	function countdown(){
+		count -=1;
+	}
+</script>
+
+{#if count == 1}
+	<h1>
+		I have {count} dollar.
+	</h1>
+{/if}
+{#if count != 1}
+	<h1>
+		I have {count} dollars.
+	</h1>
+{/if}
+
+<div>
+	<button on:click={countup}>
+		+1
+	</button>
+	<button on:click={countdown}>
+		-1
+	</button>
+</div>
